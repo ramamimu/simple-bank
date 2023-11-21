7cc0e9ccbb8a7fb2e9f540111919b467ed182338
ramamimu <ahhmaad.blogger@gmail.com>
2023-11-21 22:47:44 +0700

    feat: test async transfer using transaction

--- db/txtransfer_test.go
+++ db/txtransfer_test.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"fmt"
+	"time"
 )
 
 func (ac *AccountTest) TestTransferTx() {
@@ -64,98 +66,44 @@ func (ac *AccountTest) TestTransferTx() {
 	}
 }
 
-// func (ac *AccountTest) TestAsyncTransferTx() {
+func (ac *AccountTest) TestAsyncTransferTx() {
 
-// 	n := 2
+	n := 20
 
-// 	errTx := make(chan error)
-// 	trfTx := make(chan TransferTxResult)
-// 	accFromTx := make(chan Account)
-// 	accToTx := make(chan Account)
-// 	preAccFromTx := make(chan Account)
-// 	preAccToTx := make(chan Account)
-// 	amountTx := make(chan int64)
-// 	process := make(chan int)
-// 	// asynchronous transfer
-// 	for i := 0; i < n; i++ {
-// 		j := i
-// 		go func() {
-// 			process <- j
-// 			id1 := int64((j + 1) % defaultAccount)
-// 			id2 := int64((j + 3) % defaultAccount)
-// 			if id1 == 0 {
-// 				id1++
-// 			} else if id2 == 0 {
-// 				id2++
-// 			}
+	// test deadlock potential
+	errTx := make(chan error)
 
-// 			fmt.Printf("from %d, to %d\n\n", id1, id2)
+	// asynchronous transfer
+	for i := 0; i < n; i++ {
+		j := i
+		go func() {
+			// process <- j
+			id1 := int64((j + 1) % defaultAccount)
+			id2 := int64((j + 3) % defaultAccount)
+			if id1 == 0 {
+				id1++
+			} else if id2 == 0 {
+				id2++
+			}
 
-// 			preAccFrom, errPreAccFrom := trx.GetAccount(context.Background(), id1)
-// 			preAccTo, errPreAccTo := trx.GetAccount(context.Background(), id2)
-// 			ac.NoError(errPreAccFrom)
-// 			ac.NoError(errPreAccTo)
+			amount := int64(((j + 1) * 100) % 38)
 
-// 			amount := int64(((j + 1) * 100) % 38)
+			time.Sleep((time.Duration(amount)%10 + 1) * time.Second)
+			_, err := trx.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: id1,
+				ToAccountID:   id2,
+				Amount:        amount,
+			})
+			errTx <- err
 
-// 			tx, err := trx.TransferTx(context.Background(), TransferTxParams{
-// 				FromAccountID: id1,
-// 				ToAccountID:   id2,
-// 				Amount:        amount,
-// 			})
-
-// 			accFrom, errAccFrom := trx.GetAccount(context.Background(), id1)
-// 			accTo, errAccTo := trx.GetAccount(context.Background(), id2)
-
-// 			ac.NoError(errAccFrom)
-// 			ac.NoError(errAccTo)
-
-// 			errTx <- err
-// 			trfTx <- tx
-// 			accFromTx <- accFrom
-// 			accToTx <- accTo
-// 			preAccFromTx <- preAccFrom
-// 			preAccToTx <- preAccTo
-// 			amountTx <- amount
-// 		}()
-// 	}
-
-// 	for i := 0; i < n; i++ {
-// 		fmt.Println("Process ", <-process)
-// 		amount := <-amountTx
-// 		err := <-errTx
-// 		trf := <-trfTx
-
-// 		ac.NoError(err)
-
-// 		preAccFrom := <-preAccFromTx
-// 		ac.Equal(trf.FromAccount.Balance+amount, preAccFrom.Balance)
-// 		ac.Equal(trf.FromAccount.ID, preAccFrom.ID)
-// 		ac.Equal(trf.FromAccount.Owner, preAccFrom.Owner)
-// 		ac.Equal(trf.FromAccount.Currency, preAccFrom.Currency)
-// 		ac.Equal(trf.FromAccount.CreatedAt, preAccFrom.CreatedAt)
-
-// 		preAccTo := <-preAccToTx
-// 		ac.Equal(trf.ToAccount.Balance-amount, preAccTo.Balance)
-// 		ac.Equal(trf.ToAccount.ID, preAccTo.ID)
-// 		ac.Equal(trf.ToAccount.Owner, preAccTo.Owner)
-// 		ac.Equal(trf.ToAccount.Currency, preAccTo.Currency)
-// 		ac.Equal(trf.ToAccount.CreatedAt, preAccTo.CreatedAt)
-
-// 		accFrom := <-accFromTx
-// 		ac.Equal(trf.FromAccount.Balance-amount, accFrom.Balance)
-// 		ac.Equal(trf.FromAccount.ID, accFrom.ID)
-// 		ac.Equal(trf.FromAccount.Owner, accFrom.Owner)
-// 		ac.Equal(trf.FromAccount.Currency, accFrom.Currency)
-// 		ac.Equal(trf.FromAccount.CreatedAt, accFrom.CreatedAt)
-
-// 		accTo := <-accToTx
-// 		ac.Equal(trf.FromAccount.Balance+amount, accTo.Balance)
-// 		ac.Equal(trf.FromAccount.ID, accTo.ID)
-// 		ac.Equal(trf.FromAccount.Owner, accTo.Owner)
-// 		ac.Equal(trf.FromAccount.Currency, accTo.Currency)
-// 		ac.Equal(trf.FromAccount.CreatedAt, accTo.CreatedAt)
-
-// 		fmt.Println("Process Finished")
-// 	}
-// }
+			ac.NoError(err)
+		}()
+	}
+	for i := 0; i < n; i++ {
+		err := <-errTx
+		if err != nil {
+			fmt.Println(err)
+		}
+		ac.NoError(err)
+	}
+}
